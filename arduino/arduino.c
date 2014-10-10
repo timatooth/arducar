546fb1c397d739d9ff1725ec1c3d3c631f75af2e
Tim Sullivan <tsullivan@timatooth.com>
2014-10-10 15:22:59 +1300

    forward, left, right, backward etc

--- arduino/arduino.c
+++ arduino/arduino.c
@@ -1,18 +1,51 @@
-#define HORN_PIN 13
+#define HORN_PIN 8
+#define FORWARD_PIN 12
+#define BACKWARD_PIN 11
+#define LEFT_PIN 10
+#define RIGHT_PIN 9
+#define DELAY 100
 
 char incomingByte = 0;
 
 void setup(){
   Serial.begin(9600);
   pinMode(HORN_PIN, OUTPUT);
+  pinMode(FORWARD_PIN, OUTPUT);
+  pinMode(BACKWARD_PIN, OUTPUT);
+  pinMode(LEFT_PIN, OUTPUT);
+  pinMode(RIGHT_PIN, OUTPUT);
 }
 
 void horn(){
-  Serial.println("Blowing the horn");
   digitalWrite(HORN_PIN, HIGH);
-  delay(1000);
+  delay(DELAY);
   digitalWrite(HORN_PIN, LOW);
 }
+
+void forward(){
+  digitalWrite(FORWARD_PIN, HIGH);
+  delay(DELAY);
+  digitalWrite(FORWARD_PIN, LOW);
+}
+
+void backward(){
+  digitalWrite(BACKWARD_PIN, HIGH);
+  delay(DELAY);
+  digitalWrite(BACKWARD_PIN, LOW);
+}
+
+void left(){
+  digitalWrite(LEFT_PIN, HIGH);
+  delay(DELAY);
+  digitalWrite(LEFT_PIN, LOW);
+}
+
+void right(){
+  digitalWrite(RIGHT_PIN, HIGH);
+  delay(DELAY);
+  digitalWrite(RIGHT_PIN, LOW);
+}
+
 
 void loop(){
   if(Serial.available() > 1){
@@ -20,6 +53,18 @@ void loop(){
     switch(incomingByte){
     case 'H':
       horn();
+      break;
+    case 'F':
+      forward();
+      break;
+    case 'B':
+      backward();
+      break;
+    case 'L':
+      left();
+      break;
+    case 'R':
+      right();
       break;
     }
   }
--- server/server.c
+++ server/server.c
@@ -8,64 +8,65 @@
 #include "arduino-serial-lib.h"
 
 int main(void) {
-int serialFD;
+  int serialFD;
 
-/* Net stuff */
-int sockfd, newsockfd;
-socklen_t clilen;
-char buffer[256];
-struct sockaddr_in serv_addr, cli_addr;
-int n;
-int iSetOption = 1;
+  /* Net stuff */
+  int sockfd, newsockfd;
+  int port = 9070;
+  socklen_t clilen;
+  char buffer[256];
+  struct sockaddr_in serv_addr, cli_addr;
+  int iSetOption = 1;
 
-/* init arduino serial */
- serialFD = serialport_init("/dev/ttyUSB0", 9600);
+  /* init arduino serial */
+  serialFD = serialport_init("/dev/ttyUSB0", 9600);
   
- /* init net */
- sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  /* init net */
+  sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
- memset(&serv_addr, 0, sizeof(serv_addr));
- serv_addr.sin_family = AF_INET;
- serv_addr.sin_addr.s_addr = INADDR_ANY;
- serv_addr.sin_port = htons(9090);
- if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){ 
-   fprintf(stderr, "ERROR on binding port 9090");
- }
- setsockopt(sockfd,
-	    SOL_SOCKET,
-	    SO_REUSEADDR,
-	    (char*)&iSetOption,
-	    sizeof(iSetOption));
- /* wait for connection (this blocks) */
- while(1){
-   listen(sockfd,5);
-   clilen = sizeof(cli_addr);
-   newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-   fprintf(stderr, "New connection\n");
-   if (newsockfd < 0){
-     fprintf(stderr, "ERROR on accept connection\n");
-   }
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(port);
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){ 
+    fprintf(stderr, "ERROR on binding port %d\n", port);
+  }
+  setsockopt(sockfd,
+	     SOL_SOCKET,
+	     SO_REUSEADDR,
+	     (char*)&iSetOption,
+	     sizeof(iSetOption));
+  /* wait for connection (this blocks) */
+  while(1){
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    fprintf(stderr, "New connection\n");
+    if (newsockfd < 0){
+      fprintf(stderr, "ERROR on accept connection\n");
+    }
 
-   memset(buffer, 0, 256);
-   while(read(newsockfd, buffer, 256) > 0){
-     serialport_writebyte(serialFD, buffer[0]);
-     fprintf(stderr, "Writing byte\n");
-     if(buffer[0] == 'X'){
-       fprintf(stderr, "Client sent shutdown instruction\n");
-       goto END;
-     }
-   }
-
-   printf("Here is the message: %s\n",buffer);
-   n = write(newsockfd,"I got your message",18);
-   if (n < 0){
-     fprintf(stderr, "ERROR writing to socket");
-   }
- }
+    memset(buffer, 0, 256);
+    while(read(newsockfd, buffer, 256) > 0){
+      serialport_writebyte(serialFD, buffer[0]);
+      fprintf(stderr, "Writing byte: %c\n", buffer[0]);
+      if(buffer[0] == 'X'){
+	fprintf(stderr, "Client sent shutdown instruction\n");
+	goto END;
+      }
+    }
+    /*
+      printf("Here is the message: %s\n",buffer);
+      n = write(newsockfd,"I got your message",18);
+      if (n < 0){
+      fprintf(stderr, "ERROR writing to socket\n");
+      }
+    */
+  }
  END:
- close(newsockfd);
- close(sockfd);
- serialport_close(serialFD);
+  close(newsockfd);
+  close(sockfd);
+  serialport_close(serialFD);
   
- return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
